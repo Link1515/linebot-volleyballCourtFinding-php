7f3f0b2d6e7e68d19920b6ac27c32e9e44e399bb
Link <h26281660@gmail.com>
2024-01-10 21:25:48 +0800

    create SportsFieldInfo model

---
+++ src/LineBot/Model/SportsFieldInfo.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lynk\LineBot\Model;
+
+/**
+ * @property int $GymID
+ * @property string $Name
+ * @property string $OperationTel
+ * @property string $Address
+ * @property int $Rate 
+ * @property int $RateCount
+ * @property int $Distance 
+ * @property string $GymFuncList
+ * @property string $Photo1 
+ * @property string $LatLng 
+ * @property string $RentState 
+ * @property string $OpenState 
+ * @property string $Declaration
+ * @property string|null $LandAttrName
+ * @property float|null  $distance 
+ */
+class SportsFieldInfo
+{
+}
