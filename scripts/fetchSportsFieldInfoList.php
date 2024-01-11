0019b509eca49353179e65e89d7f176bf00cc0e4
Link1515 <h26281660@gmail.com>
2024-01-11 09:00:58 +0800

    fix fetchSportsFieldInfoList script create data dir when it is not exists

--- scripts/fetchSportsFieldInfoList.php
+++ scripts/fetchSportsFieldInfoList.php
@@ -6,10 +6,15 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $client = new GuzzleHttp\Client();
 $apiUrl = 'https://iplay.sa.gov.tw/api/GymSearchAllList?$format=application/json;odata.metadata=none&Keyword=%E6%8E%92%E7%90%83%E5%A0%B4';
-$filePath = __DIR__ . '/../data/sportsFieldInfoList.json';
+$dirPath = __DIR__ . '/../data';
+$filename = 'sportsFieldInfoList.json';
 
 $res = $client->request('GET', $apiUrl);
 
-file_put_contents($filePath, $res->getBody()->getContents());
+if (!is_dir($dirPath)) {
+    mkdir($dirPath);
+}
+
+file_put_contents($dirPath . '/' . $filename, $res->getBody()->getContents());
 
 echo 'ok';
