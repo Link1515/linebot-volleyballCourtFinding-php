96d5bb717601b3642b426f39885eb41af20f66ac
Link1515 <terrylin1515@gmail.com>
2026-01-20 16:15:53 +0800

    chore: remove root hello route

--- config/routes.php
+++ config/routes.php
@@ -2,17 +2,9 @@
 
 declare(strict_types=1);
 
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
 use Slim\App;
 use TerryLin\LineBot\Controller\WebhookController;
 
 return function (App $app): void {
     $app->post('/webhook', WebhookController::class);
-
-    $app->get('/', function (RequestInterface $req, ResponseInterface $res) {
-        $res->getBody()->write('hello line');
-
-        return $res;
-    });
 };
