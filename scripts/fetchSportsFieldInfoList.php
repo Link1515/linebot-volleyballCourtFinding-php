c8ca7fed81fe103fa8bf56419384bc6712365c52
Link1515 <terrylin1515@gmail.com>
2026-01-21 09:41:48 +0800

    fix: update sports api url

--- scripts/fetchSportsFieldInfoList.php
+++ scripts/fetchSportsFieldInfoList.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $client   = new GuzzleHttp\Client();
-$apiUrl   = 'https://iplay.sa.gov.tw/api/GymSearchAllList?$format=application/json;odata.metadata=none&Keyword=%E6%8E%92%E7%90%83%E5%A0%B4';
+$apiUrl   = 'https://iplay.sports.gov.tw/api/GymSearchAllList?$format=application/json;odata.metadata=none&Keyword=排球場';
 $dirPath  = __DIR__ . '/../storage/data';
 $filename = 'sportsFieldInfoList.json';
 
